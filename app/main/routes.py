a76fd3e4fb9ac61aa77519045f73d3157f2c2237
Jaroslav Kafka <jaroslav.kafka@elso-systems.com>
2022-11-14 15:43:31 +0100

    insert or update data

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,28 @@
 from flask import Blueprint, render_template, redirect, url_for, request
+from apscheduler.schedulers.background import BackgroundScheduler
+import urllib.request, json 
 
 from app.extensions import mongo
+
+def find_and_update_data():
+    movies_connection = mongo.db.movies
+    with urllib.request.urlopen("https://gist.githubusercontent.com/nextsux/f6e0327857c88caedd2dab13affb72c1/raw/04441487d90a0a05831835413f5942d58026d321/videos.json") as url:
+        datas = json.load(url)
+        for data in datas:
+            try:
+                movies_connection.find_one_or_404({"name":data["name"]})
+            except:
+                movies_connection.insert_one(data)
+                
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=find_and_update_data, trigger="interval", seconds=300)
+scheduler.start()
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    find_and_update_data()
     return render_template('index.html')
 
 @main.route('/add_movie', methods=['POST'])
@@ -14,3 +31,4 @@ def add_movie():
     movies_connection = mongo.db.movies
     movies_connection.insert_one({"text" : movie_item})
     return redirect(url_for('main.index'))
+
